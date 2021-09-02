7b5a9af008c654f93eca3dd197ec738b63124ae1
NoCereaL <therealskm@hotmail.com>
2021-09-02 17:59:39 +0100

    Update

---
+++ adminLogin.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html><head>
+<title>Plumbing Heating Air Conditioning Services Ltd Control Panel by AMCS </title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="description" content="" />
+<meta name="copyright" content="" />
+<link rel="stylesheet" href=" https://www.skmoyana.co.uk/media/css/login.css ">
+<link href='http://fonts.googleapis.com/css?family=Telex' rel='stylesheet' type='text/css'>
+</head><body>
+
+<div class="main">
+        <div class = "logo">
+            <img src = "http://www.newworlddigitalmedia.co.uk/images/logo_cp.jpg" style="max-width:241px">
+        </div>   
+        <div class = "loginbox">
+                <form action="https://www.skmoyana.co.uk/admin/login.php" method="post" data-netlify="true" accept-charset="utf-8">     
+                <table>
+                    <tr>
+                        <td><label for="username">Username</label></td>
+                        <td><input type="text" name="username" value="" id="input_login"  /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="password">Password</label></td>
+                        <td><input type="password" name="password" value="" id="input_password"  /></td>
+                    </tr>
+                                        <tr>
+                        <td colspan = "2" class="center" ><input type="submit" name="login_button" value="Login"  /></td>
+                    </tr>
+                </table>
+                </form>                
+            <div class= "text">
+                    If you have forgotten your password, please call 07926311379 and request a new password.
+
+                    Please have your Domain Name, and Business Name ready.
+                </div>
+                </div>
+
+
+</div><footer>
+    Copyright � 2021 <a href ="www.skmoyana.co.uk">MoyanaSK Studios</a>, All Rights Reserved Testing Testing
+</footer>
+
+</body></html>
